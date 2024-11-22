feada7f749d45e334cc25b92297dc9d14b5933e8
Artem Sychov <artem@sychov.dev>
2024-11-22 17:21:48 +0100

    get some order after review the solution and fix test

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -25,5 +25,52 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node, node2)
 
 
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text_node_to_raw_text(self):
+        text_node = TextNode("Plain text", TextType.TEXT)
+        html_node = TextNode.text_node_to_html_node(text_node)
+        self.assertEqual(html_node.to_html(), "Plain text")
+
+    def test_text_node_to_bold(self):
+        text_node = TextNode("Bold text", TextType.BOLD)
+        html_node = TextNode.text_node_to_html_node(text_node)
+        self.assertEqual(html_node.to_html(), "<b>Bold text</b>")
+
+    def test_text_node_to_italic(self):
+        text_node = TextNode("Italic text", TextType.ITALIC)
+        html_node = TextNode.text_node_to_html_node(text_node)
+        self.assertEqual(html_node.to_html(), "<i>Italic text</i>")
+
+    def test_text_node_to_code(self):
+        text_node = TextNode("Code snipet", TextType.CODE)
+        html_node = TextNode.text_node_to_html_node(text_node)
+        self.assertEqual(html_node.to_html(), "<code>Code snipet</code>")
+
+    def test_text_node_to_link(self):
+        text_node = TextNode("Boot.dev", TextType.LINK, url="https://www.boot.dev")
+        html_node = TextNode.text_node_to_html_node(text_node)
+        self.assertEqual(html_node.to_html(), '<a href="https://www.boot.dev">Boot.dev</a>')
+
+    def test_text_node_to_image(self):
+        text_node = TextNode("Alt text", TextType.IMAGE, url="https://example.com/image.png")
+        html_node = TextNode.text_node_to_html_node(text_node)
+        self.assertEqual(html_node.tag, 'img')
+        self.assertEqual(html_node.value, '')
+        self.assertEqual(html_node.props, {"src": "https://example.com/image.png", "alt": "Alt text"})
+
+    def test_text_node_missing_url(self):
+        text_node = TextNode("Alt text", TextType.IMAGE)
+        with self.assertRaises(ValueError):
+            TextNode.text_node_to_html_node(text_node)
+
+    def test_text_node_unsupported_type(self):
+        class UnsupportedType:
+            pass
+
+        text_node = TextNode("unsupported", UnsupportedType)
+        with self.assertRaises(Exception):
+            TextNode.text_node_to_html_node(text_node)
+
+
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,4 @@
+from htmlnode import LeafNode
 from enum import Enum
 
 
@@ -28,3 +29,24 @@ class TextNode():
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+    def text_node_to_html_node(text_node):
+        match text_node.text_type:
+            case TextType.TEXT:
+                return LeafNode(None, text_node.text)
+            case TextType.BOLD:
+                return LeafNode("b", text_node.text)
+            case TextType.ITALIC:
+                return LeafNode("i", text_node.text)
+            case TextType.CODE:
+                return LeafNode("code", text_node.text)
+            case TextType.LINK:
+                if not text_node.url:
+                    raise ValueError("TextNode of type LINK must have a URL.")
+                return LeafNode("a", text_node.text, {"href": text_node.url})
+            case TextType.IMAGE:
+                if not text_node.url:
+                    raise ValueError("TextNode of type IMAGE must have a URL.")
+                return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+            case _:
+                raise Exception(f"Unsupported TextType: {text_node.text_type}")
